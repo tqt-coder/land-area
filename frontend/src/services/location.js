d8c7fa8074feded04c13b67950ec840a5c62fbff
Quoc Tuan <tranquocuan310120021@gmail.com>
2024-05-10 23:16:04 +0700

    call api

--- frontend/src/services/location.js
+++ frontend/src/services/location.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 
+
 const LocationService = {
   /**
    * Get all provinces in Viet Name
    * @returns {Promise<{idProvince: string, name: string, id: string}[]>}
    */
   getProvincesOrCities: async () => {
-    const { data } = await axios.get("/provinces");
-    console.log(data);
-    return data;
+    // const { data } = await axios.get("/provinces");
+    try {
+      // Make a GET request to another API endpoint
+      const response = await axios.get("http://127.0.0.1:5000/provinces");
+      console.log(response);
+      return response.data; // Return the data from the response
+    } catch (error) {
+      // Handle any errors that occur during the API call
+      console.error("Error calling other API:", error);
+      throw error; // Re-throw the error to propagate it to the caller
+    }
+
   },
   /**
    *
