391e0c9258646d45ac1661ef87f7402c2c13c323
Quoc Tuan <tranquocuan310120021@gmail.com>
2024-06-09 22:25:12 +0700

    update UI

--- frontend/src/views/forgetPassword.js
+++ frontend/src/views/forgetPassword.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { forgotPassword } from "../services/authService";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 import "../assets/demo/login.css";
-
 
 const ForgotPassword = () => {
   const navigate = useNavigate();
-  const [setError] = useState("");
-  const [setCategory] = useState("");
+  const [error, setError] = useState(""); // Destructuring both state and setter function
+  const [category, setCategory] = useState(""); // Destructuring both state and setter function
   const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
@@ -17,13 +16,12 @@ const ForgotPassword = () => {
       const response = await forgotPassword(email);
       setCategory(response.type || "error");
       setError(response.message || "Something went wrong!!");
-      if (response.status === 200) {
-        alert(response.message)
-        navigate('/admin/login');
-      }
       setTimeout(() => {
         setError("");
-      }, 2000);
+        if (response.status === 200) {
+          navigate("/admin/login");
+        }
+      }, 5000);
     } catch (error) {
       setError("Internal Server Error");
       setCategory("error");
@@ -35,33 +33,52 @@ const ForgotPassword = () => {
 
   return (
     <>
-    <Helmet>
-        <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+      <Helmet>
+        <script
+          src="https://kit.fontawesome.com/64d58efce2.js"
+          crossorigin="anonymous"
+        ></script>
       </Helmet>
-    <div className="container">
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form onSubmit={handleSubmit} className="sign-in-form form-css">
-            <h4 className="title">Please input your email</h4>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </div>
-            <input type="submit" value="Submit" className="btn btn-danger" />
-          </form>
-        </div>
-      </div>
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
-            <form action="/admin/login" method="get" className="sign-in-form form-css">
-              <input type="submit" value="Sign In" className="btn btn-danger" />
+      <div className="container">
+        {error && <p className={`alert alert-${category}`}>{error}</p>}
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form onSubmit={handleSubmit} className="sign-in-form form-css">
+              <h4 className="title">Please input your email</h4>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <input type="submit" value="Submit" className="btn btn-danger" />
             </form>
           </div>
-          <img src="/assets/img/log.svg" className="image" alt="" />
+        </div>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <form
+                action="/admin/login"
+                method="get"
+                className="sign-in-form form-css"
+              >
+                <input
+                  type="submit"
+                  value="Sign In"
+                  className="btn btn-danger"
+                />
+              </form>
+            </div>
+            <img src="/assets/img/log.svg" className="image" alt="" />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
