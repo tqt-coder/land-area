26a1f9aab1a4b0035ac4bd52119a5cac5986ef28
Quoc Tuan <tranquocuan310120021@gmail.com>
2024-06-20 22:36:43 +0700

    format

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,27 +1,30 @@
 import axios from "axios";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 // Set the base URL for the axios instance
-const baseURL = 'http://127.0.0.1:5000';
+const baseURL = "http://127.0.0.1:5000";
 
 const instance = axios.create({
   baseURL,
   withCredentials: true, // Ensure that cookies are sent with requests
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 // Add an interceptor to attach the token cookie to the headers of each request
-instance.interceptors.request.use(config => {
-  const token = Cookies.get('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+);
 
 const DashBoardService = {
   calcArea: async (_province, _district, _ward, navigate) => {
@@ -32,7 +35,7 @@ const DashBoardService = {
         district : _district,
         ward: _ward
       });
-
+      
       console.log(response);
 
       if (response.data.status !== 200 && response.data.status !== 403) {
@@ -45,24 +48,18 @@ const DashBoardService = {
       }
 
       let xData = response.data.area;
-      let arr;
-      if (xData) {
-        arr = Object.values(xData);
-        arr.shift();
-      }
-
       let obj = {
-        'arr': arr,
-        'url': response.data.image_url
+        arr: Object.values(xData),
+        url: response.data.image_url,
       };
 
-      console.log('dashboard: ', obj);
+      console.log("dashboard: ", obj);
       return obj; // Return the data from the response
     } catch (error) {
       console.error("Error calling other API:", error);
       throw error; // Re-throw the error to propagate it to the caller
     }
-  }
+  },
 };
 
 export default DashBoardService;
